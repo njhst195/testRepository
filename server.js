2f52fe82cba37574b07d3262c5ae72e3c3ea866b
njhst195 <71187990+njhst195@users.noreply.github.com>
2021-03-04 17:20:34 -0500

    Server added and git ignore

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3003;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// listening to port
+app.listen(PORT, () => {
+    console.log("Listening to port ", PORT);
+});
